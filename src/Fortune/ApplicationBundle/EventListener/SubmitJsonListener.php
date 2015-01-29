1c935a316c15040a62d7dcc41e5b7266108da07d
Sergey Vasko <sergio.vasko@gmail.com>
2015-01-30 01:17:48 +0200

    Handled errors

--- src/Fortune/ApplicationBundle/EventListener/SubmitJsonListener.php
+++ src/Fortune/ApplicationBundle/EventListener/SubmitJsonListener.php
@@ -11,6 +11,12 @@ class SubmitJsonListener
         $request = $event->getRequest();
         $content = $request->getContent();
         $data = json_decode($content, true);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            $data = array('message' => 'Invalid or malformed JSON');
+            $response = new JsonResponse($data, Response::HTTP_BAD_REQUEST);
+            $event->setResponse($response);
+            $event->stopPropagation();
+        }
         $request->request->add($data ?: array());
     }
 }
